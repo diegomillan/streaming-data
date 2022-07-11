494bacab25d258897c07a890a356a6fa988ea3f2
Diego Millan <diegor.millan@gmail.com>
2022-07-10 21:41:00 -0500

    feat: adding minimal gamelog API

--- API-ingest/app/main.py
+++ API-ingest/app/main.py
@@ -19,14 +19,12 @@ from kafka import KafkaProducer, producer
 # Create class (schema) for the JSON
 # Date get's ingested as string and then before writing validated
 class InvoiceItem(BaseModel):
-    InvoiceNo: int
-    StockCode: str
-    Description: str
-    Quantity: int
-    InvoiceDate: str
-    UnitPrice: float
-    CustomerID: int
-    Country: str
+    Date: str
+    Opp: str
+    AB: int
+    HR: int
+    OppStarter: str
+
 
 # This is important for general execution and the docker later
 app = FastAPI()
@@ -35,3 +33,34 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World from my code"}
+
+@app.post("/gamelogitem")
+async def post_invoice_item(item: InvoiceItem):
+    print("Message received")
+    try:
+
+        # Parse item back to json
+        json_of_item = jsonable_encoder(item)
+
+        # Dump the json out as string
+        json_as_string = json.dumps(json_of_item)
+        print(json_as_string)
+
+        # Produce the string
+        produce_kafka_string(json_as_string)
+
+        # Encode the created customer item if successful into a JSON and return it to the client with 201
+        return JSONResponse(content=json_of_item, status_code=201)
+
+    # Will be thrown by datetime if the date does not fit
+    # All other value errors are automatically taken care of because of the InvoiceItem Class
+    except ValueError:
+        return JSONResponse(content=jsonable_encoder(item), status_code=400)
+
+def produce_kafka_string(json_as_string):
+    # Create producer
+        producer = KafkaProducer(bootstrap_servers='kafka:9092',acks=1)
+
+        # Write the string as bytes because Kafka needs it this way
+        producer.send('ingestion-topic', bytes(json_as_string, 'utf-8'))
+        producer.flush()
